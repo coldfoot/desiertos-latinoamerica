cd6d0bc3dfda2da51af93ae5bedeaf4863b4ce80
tiagombp <tiagombp@gmail.com>
2025-05-30 15:27:34 -0300

    adds events listener to argentina provincias

--- dashboard/script-dash.js
+++ dashboard/script-dash.js
@@ -4,6 +4,12 @@ const menu_nav_conteudo = document.querySelector(".wrapper-btns-nav");
 const expand_button_mobile = document.querySelector(".expand-card-mobile");
 const text_panel = document.querySelector(".text-panel-container");
 
+const current_place = {
+    country : '',
+    provincia : '',
+    localidad : ''
+}
+
 menu_tipo_paisage.addEventListener("click", e => {
 
     const div_tipo_paisage = e.target.closest('.menu-tipo-paisage > div[data-tipo-paisage]');
@@ -359,7 +365,7 @@ function render_place(pais) {
     update_breadcrumbs("pais", pais);
     update_infocard(pais);
 
-    render_country_subnational(pais);
+    render_country_subnational(pais); // os eventos do subnacional estao aqui dentro
     monitor_events('off'); // desliga monitor de eventos no nível de país
 
 }
@@ -380,8 +386,25 @@ function render_country_subnational(pais) {
             '#666'
         );
 
+        provincias_argentina.monitor_events("on");
+
     } else {
         console.log("No data yet.")
+
+        map.setPaintProperty(
+            'localidad',
+            'fill-color', 
+            'transparent'
+        );
+
+        map.setPaintProperty(
+            'provincia-border-hover',
+            'line-color',
+            'transparent'
+        );
+
+        provincias_argentina.monitor_events("off");
+
     }
 
 }
@@ -550,6 +573,175 @@ function load_provincias_argentina() {
             'line-width': 4
         },
         'filter': ['==', 'provincia', '']}); // puts behind road-label
+
+}
+
+function toggle_hightlight_border_provincia_argentina(provincia) {
+
+    map.setFilter(
+        'provincia-border', [
+            '==',
+            ['get', 'nam'],
+            provincia
+        ]
+    );
+
+
+}
+
+function render_provincia_argentina(provincia) {
+
+    console.log(provincia);
+
+};
+
+const provincias_argentina = {
+
+    hoveredStateId : null,
+
+    popup: new mapboxgl.Popup(
+        {
+            closeButton: false,
+            loseOnClick: false
+        }),
+
+    mouse_enter_handler : function (e) {
+
+        // pop up
+        let coordinates = [
+            e.features[0].properties.xc,
+            e.features[0].properties.yc
+        ]; 
+
+        let name = e.features[0].properties.nam;
+
+        provincias_argentina.popup.setLngLat(coordinates).setHTML(name).addTo(map);
+
+        // precisa desse if aqui para fazer tirar o estado de hover da provincia anterior quando passa para outra provincia
+
+        if (provincias_argentina.hoveredStateId) {
+            map.setFeatureState(
+                { 
+                    source: 'provincia',
+                    sourceLayer: 'provincia',
+                    id: provincias_argentina.hoveredStateId
+                },
+
+                { hover : false }
+            )
+
+        }
+
+        provincias_argentina.hoveredStateId = e.features[0].properties.nam;
+
+        map.setFeatureState(
+            { 
+                source: 'provincia',
+                sourceLayer: 'provincia',
+                id: provincias_argentina.hoveredStateId
+            },
+
+            { hover : true }
+        )
+
+        //console.log('Hover no ', hoveredStateId)
+
+        // algo mais a fazer aqui
+
+    },
+
+    mouse_leave_handler : function () {
+
+        provincias_argentina.popup.remove();
+
+        if (provincias_argentina.hoveredStateId) {
+            map.setFeatureState(
+                { 
+                    source: 'provincia', 
+                    sourceLayer: 'provincia',
+                    id: provincias_argentina.hoveredStateId 
+                },
+
+                { hover: false }
+            );
+        }
+    
+        provincias_argentina.hoveredStateId = null;
+    },
+
+    click_event_handler : function(e) {
+
+        const province_name = e.features[0].properties.nam;
+
+        /* Para evitar re-renderizar quando clica na mesma província
+        if (province_name != current_place.provincia) {
+
+            const local = {
+
+                local : province_name,
+                tipo  : "provincia",
+                text  : province_name
+
+            };
+        */
+
+        // clears hover featureState
+        // o id da da provincia é o nam. Só ver o 'promoteId' no addSource lá em cima.
+        map.setFeatureState(
+            { 
+                source: 'provincia',
+                sourceLayer: 'provincia',
+                id: province_name
+            },
+
+            { hover : false }
+        );
+
+        render_provincia_argentina(province_name);
+
+    },
+
+    monitor_events : function(option) {
+
+        if (option == 'on') {
+
+            if (provincias_argentina.hoveredStateId) {
+                map.setFeatureState(
+                    { 
+                        source: 'provincia',
+                        sourceLayer: 'provincia',
+                        id: provincias_argentina.hoveredStateId 
+                    },
+
+                    { hover: false }
+                );
+            }
+
+            //dash.map.localidad.hoveredStateId = null;
+
+            map.on('mousemove', 'provincia', provincias_argentina.mouse_enter_handler);
+
+            map.on('mouseleave', 'provincia', provincias_argentina.mouse_leave_handler);
+
+            map.on('click', 'provincia', provincias_argentina.click_event_handler);
+
+            // como tem o layer aqui, dá para no handler pegar o e.features!
+
+        } else {
+
+            //console.log('turning off province event monitor');
+
+            map.off('mousemove', 'provincia', provincias_argentina.mouse_enter_handler);
+
+            map.off('mouseleave', 'provincia', provincias_argentina.mouse_leave_handler);
+
+            map.off('click', 'provincia', provincias_argentina.click_event_handler);
+
+            //dash.map.province.hoveredStateId = null;
+            
+        }
+
+    }
 
 }
 
