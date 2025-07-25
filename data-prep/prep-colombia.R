596ef6c30e5b6df46dec8e76f1051889a2a3a301
tiagombp <tiagombp@gmail.com>
2025-07-25 07:48:07 -0300

    updates colombia bbox

--- data-prep/prep-colombia.R
+++ data-prep/prep-colombia.R
@@ -10,3 +10,5 @@ col_centers <- st_centroid(colombia) %>% filter(CLASSIFICATION != "SIN DATOS")
 ggplot() + geom_sf(data = colombia) + geom_sf(data = col_centers)
 
 st_write(col_centers, "colombia-centroids.geojson", driver = "GeoJSON")
+
+st_bbox(colombia)
